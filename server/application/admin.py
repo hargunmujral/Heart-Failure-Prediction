a611135d8f8776b42da26cbbe057435f8e5b61f9
hargunmujral <83234565+hargunmujral@users.noreply.github.com>
2022-08-06 13:14:59 -0400

    added admin page

--- server/application/admin.py
+++ server/application/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import HeartHealthInformation
 
+class HeartHealthInformationAdmin(admin.ModelAdmin):
+    list_display = ('age', 'anemia', 'cpk', 'diabetes', 'ejection_fraction', 'high_blood_pressure', 'platelets',
+     'serum_creatinine', 'serum_sodium', 'sex', 'smoking', 'time', 'death')
+    
 # Register your models here.
+admin.site.register(HeartHealthInformation, HeartHealthInformationAdmin)
--- server/application/models.py
+++ server/application/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+
+class HeartHealthInformation(models.Model):
+    patient_name = models.CharField(max_length=100)
+    age = models.BigIntegerField()
+    anemia = models.BigIntegerField()
+    cpk = models.BigIntegerField()
+    diabetes =  models.BigIntegerField()
+    ejection_fraction = models.BigIntegerField()
+    high_blood_pressure =  models.BigIntegerField()
+    platelets = models.BigIntegerField()
+    serum_creatinine =  models.BigIntegerField()
+    serum_sodium = models.BigIntegerField()
+    sex =  models.BigIntegerField()
+    smoking = models.BigIntegerField()
+    time = models.BigIntegerField()
+    death = models.BooleanField()
+
+#str
+    def __str__(self):
+        return self.patient_name
