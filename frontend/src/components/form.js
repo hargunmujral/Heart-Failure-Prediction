0afe8334e5bfef9d2476a59a6f82d716be71cfa1
hargunmujral <83234565+hargunmujral@users.noreply.github.com>
2022-08-07 23:25:22 -0400

    adjusted some fields in the form

--- frontend/src/components/form.js
+++ frontend/src/components/form.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Formik, Form } from 'formik';
-import { Button, TextField } from '@mui/material';
+import { Button, Checkbox, TextField } from '@mui/material';
 import { makeStyles } from '@mui/styles';
 import * as Yup from 'yup';
 
 const useStyles = makeStyles(() => ({
     form: {
+        display: 'flex',
+        flexDirection: 'column',
         alignItems: 'center',
         justifyContent: 'center',
         width: '100%',
@@ -24,6 +26,7 @@ export default function SubmitForm() {
     const classes = useStyles();
     
     const initialValues = {
+        Patient_Name: '',
         Age: '',
         Anemia: '',
         CPK: '',
@@ -34,19 +37,24 @@ export default function SubmitForm() {
         Serum_Creatinine: '',
         Serum_Sodium:'',
         Sex: '',
+        Smoking: '',
+        Time: '',
     };
 
     const validationSchema = Yup.object().shape({
+        Patient_Name: Yup.string().required('Patient Name field is required'),
         Age: Yup.string().required('Age field is required'),
-        Anemia: Yup.string().required('Anemia field is required'),
+        Anemia: Yup.boolean(),
         CPK: Yup.string().required('CPK field is required'),
-        Diabetes: Yup.string().required('DIabetes field is required'),
+        Diabetes: Yup.boolean(),
         Ejection_Fraction: Yup.string().required('Ejection Fraction field is required'),
-        High_Blood_Pressure: Yup.string().required('High Blood Pressure field is required'),
+        High_Blood_Pressure: Yup.boolean(),
         Platelets: Yup.string().required('Platelets field is required'),
         Serum_Creatinine: Yup.string().required('Serum Creatinine field is required'),
         Serum_Sodium: Yup.string().required('Serum Sodium field is required'),
-        Sex: Yup.string().required('Sex field is required')
+        Sex: Yup.string().required('Sex field is required'),
+        Smoking: Yup.boolean(),
+        Time: Yup.string().required('Time field is required')
     });
 
     const handleSubmit = () => {
@@ -62,16 +70,42 @@ export default function SubmitForm() {
         >
             {({ values, errors, touched, handleChange, handleSubmit, isSubmitting }) => (
                 <Form className={classes.form} onChange={handleChange} onSubmit={handleSubmit}>
+                    <TextField name="Patient_Name" label="Patient Name" style={{ paddingBottom: '10px' }} />
                     <TextField name="Age" label="Age" value={values.Age} style={{ paddingBottom: '10px' }}/>
-                    <TextField name="Anemia" label="Anemia" value={values.Anemia} style={{ paddingBottom: '10px' }}/>
                     <TextField name="CPK" label="CPK" value={values.CPK} style={{ paddingBottom: '10px' }} />
-                    <TextField name="Diabetes" label="Diabetes" value={values.Diabetes} style={{ paddingBottom: '10px' }} />
                     <TextField name="Ejection_Fraction" label="Ejection Fraction" value={values.Ejection_Fraction} style={{ paddingBottom: '10px' }} />
-                    <TextField name="High_Blood_Pressure" label="High Blood Pressure" value={values.High_Blood_Pressure} style={{ paddingBottom: '10px' }} />
                     <TextField name="Platelets" label="Platelets" value={values.Platelets} style={{ paddingBottom: '10px' }} />
                     <TextField name="Serum_Creatinine" label="Serum Creatinine" value={values.Serum_Creatinine} style={{ paddingBottom: '10px' }} />
                     <TextField name="Serum_Sodium" label="Serum Sodium" value={values.Serum_Sodium} style={{ paddingBottom: '10px' }}  />
-                    <TextField name="Sex" label="Sex" value={values.Sex} style={{ paddingBottom: '10px' }} />
+                    <TextField name="Time" label="Time" value={values.Time} style={{ paddingBottom: '10px' }} />
+                    <label>
+                        Diabetes
+                        <Checkbox name="Diabetes" value={values.Diabetes} style={{ paddingBottom: '10px' }} />                   
+                    </label>
+                    <label>
+                        High Blood Pressure
+                        <Checkbox name="High_Blood_Pressure" value={values.High_Blood_Pressure} style={{ paddingBottom: '10px' }} />
+                    </label>
+                    <label>
+                        Anemia
+                        <Checkbox name="Anemia" value={values.Anemia} style={{ paddingBottom: '10px' }}/>
+                    </label>
+                    <label>
+                        Smoking
+                        <Checkbox name="Smoking" value={values.Smoking} style={{ paddingBottom: '10px' }}/>
+                    </label>
+                    <label>
+                        Sex
+                        <select
+                            name="Sex"
+                            value={values.Sex}
+                            onChange={handleChange}
+                            style={{ display: "block" }}
+                        >
+                            <option value="Male" label="Male">Male</option>
+                            <option value="Female" label="Female">Female</option>
+                        </select>
+                    </label>
                     <br />
                     <Button 
                         type="submit" variant="contained" color="primary"
